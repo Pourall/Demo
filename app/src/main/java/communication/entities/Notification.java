31895046bb3e069af10311ccccbf255d562be3db
Youping Fan <Youping Fan>
2016-08-14 20:46:30 +0800

    create Jsonable

--- app/src/main/java/communication/entities/Notification.java
+++ app/src/main/java/communication/entities/Notification.java
@@ -1,5 +1,10 @@
 package communication.entities;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -8,7 +13,7 @@ import java.util.Map;
 /**
  *通知Java类
  */
-public class Notification {
+public class Notification implements Jsonable{
 
     private String id;
     private String title;
@@ -65,7 +70,7 @@ public class Notification {
         this.receivers = receivers;
     }
 
-    public Map getMap(){
+    private Map getMap(){
         Map<String,Object> map =new HashMap<String,Object>();
         map.put("id",id);
         map.put("title",title);
@@ -79,5 +84,35 @@ public class Notification {
     @Override
     public String toString(){
         return "notification";
+    }
+
+    @Override
+    public JSONObject toJson(Object object) {
+        return new JSONObject(this.getMap());
+    }
+
+    /**
+     * 将JSON数据转化为Java对象
+     */
+    public static Object readFromJson(String json){
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JsonError","Not JSONObject");
+        }
+        Notification notification =new Notification();
+        try {
+            notification.setId((String) jsonObject.get("id"));
+            notification.setTitle((String) jsonObject.get("title"));
+            notification.setContent((String) jsonObject.get("content"));
+            notification.setPubliishDate((Date) jsonObject.get("publishDate"));
+            notification.setSender((String) jsonObject.get("sender"));
+            notification.setReceivers((List) jsonObject.get("receiver"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return notification;
+
     }
 }
--- app/src/main/java/communication/entities/Student.java
+++ app/src/main/java/communication/entities/Student.java
@@ -1,12 +1,17 @@
 package communication.entities;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 学生Java类
  */
-public class Student {
+public class Student implements Jsonable{
 
     private String id;
 
@@ -14,20 +19,8 @@ public class Student {
 
     private String character;
 
-    /*
-    private volatile static Student student = null;
-    private Student(){}
-    public static  Student getInstance(){
-        if(student == null) {                         //Single Checked
-            synchronized (Student.class) {
-                if (student == null) {                 //Double Checked
-                    student = new Student();
-                }
-            }
-        }
-        return student ;
-    }
-    */
+    private String password;
+
     public String getId() {
         return id;
     }
@@ -52,7 +45,11 @@ public class Student {
         this.character = character;
     }
 
-    public Map getMap(){
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    private Map getMap(){
         Map<String,Object> map =new HashMap<String,Object>();
         map.put("id",id);
         map.put("name",name);
@@ -61,8 +58,33 @@ public class Student {
     }
 
     @Override
+    public JSONObject toJson(Object object) { return new JSONObject(this.getMap());}
+
+    @Override
     public String toString(){
         return "student";
+    }
+
+    /**
+     * 将JSON数据转化为Java对象
+     */
+    public static Object readFromJson(String json){
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JsonError","Not JSONObject");
+        }
+        Student student = new Student();
+        try {
+            student.setId((String) jsonObject.get("id"));
+            student.setCharacter((String) jsonObject.get("character"));
+            student.setName((String) jsonObject.get("name"));
+            student.setPassword((String) jsonObject.get("password"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return student;
     }
 
 }
